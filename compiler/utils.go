d955e13a91a8b647377832e9ebd4cd3db18a5fd6
Yusuf Simonson <simonson@gmail.com>
2016-01-22 10:31:49 -0500

    Watch for changes in .css files as well

--- compiler/utils.go
+++ compiler/utils.go
@@ -7,12 +7,18 @@ import (
 // Returns whether the given file is sass
 func isSassFile(path string) bool {
 	ext := filepath.Ext(path)
-	return ext == ".scss"
+	return ext == ".scss" || ext == ".css"
 }
 
-// Returns whether the given file is private (i.e. whether the filename starts
-// with an underscore)
+// Returns whether the given file is private (i.e. whether it's included by
+// root sass files)
 func isPrivateFile(path string) bool {
 	base := filepath.Base(path)
-	return len(base) > 0 && base[0] == '_'
+	ext := filepath.Ext(path)
+
+	if ext == ".scss" {
+		return len(base) > 0 && base[0] == '_'
+	} else {
+		return ext == ".css"
+	}
 }
